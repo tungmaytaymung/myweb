3d873404ec225f7041fbf04c099ce512cc460a6d
Thanyaphan Phinthuphan <tungmaytaymung@gmail.com>
2020-10-31 20:36:50 +0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#navbarNavAltMarkup a").click(function(event){  
+        if ($(".navbar-collapse").hasClass("show") && $(window).width() <= 960) {  
+            $(".navbar-collapse").slideToggle();
+        }
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var target_offset = $("#"+parts[1]).offset();
+        var target_top = (target_offset.top);
+        $('html, body').animate({scrollTop:target_top}, 100);
+        return false; 
+    });
+    $(".navbar-toggler").click(function(event) {
+        $(".navbar-collapse").slideToggle(1000);
+    });
+});
